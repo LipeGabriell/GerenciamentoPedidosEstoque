cfa78de2af0a2056a61dbd528db9a29ccb56ad41
Felipe Gabriel <felipe.ramiressanches@hotmail.com>
2025-03-11 12:08:09 -0300

    created models

---
+++ GerenciamentoPedidosEstoque/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoPedidosEstoque.Models;
+
+public class Cliente
+{
+    [Key] [Required] public int Id { get; set; }
+    [Required] public string Name { get; set; }
+    [EmailAddress] [Required] public string Email { get; set; }
+    [Required] public string Cellphone { get; set; }
+    [Required] public string Address { get; set; }
+}
